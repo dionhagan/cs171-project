7bd98370198ceada63d7b42255c2f2a39089353c
David Wihl <wihl@ripariandata.com>
2016-03-28 13:40:13 -0400

    initial project plan ready to submit

---
+++ processbook/pdf/convert.sh
@@ -0,0 +1,4 @@
+# temporary hacky way to generate one PDF from multiple iPython notebooks
+# Ref: http://gotofritz.net/blog/howto/joining-pdf-files-in-os-x-from-the-command-line/
+jupyter nbconvert --to latex ../*.ipynb
+"/System/Library/Automator/Combine PDF Pages.action/Contents/Resources/join.py" -o ../processbook.pdf *.pdf
