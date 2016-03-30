9172e562b74936fe604ba7d8d2654a85b4724a98
dionhagan <dionhagan@college.harvard.edu>
2016-03-30 17:52:29 -0400

    hooked up webservice to get first round 'sample' predictions

--- client/js/predict.js
+++ client/js/predict.js
@@ -1,5 +1,9 @@
 "use strict";
 
+var BASE_URL = "http://ws.chanceme.info/predict?admissionstest=0.976899206&AP=7&averageAP=1.06733864&SATsubject=0.324271565&GPA=-0.187109979&schooltype=0&intendedgradyear=2017&female=1&MinorityRace=0&international=0&sports=0&earlyAppl=0&alumni=0&outofstate=0&acceptrate=0.151&size=6621&public=0&finAidPct=0&instatePct=0&callback=getJSON";
+
+console.log(jsonData);
+
 function predict(formData) {
   // TODO: call webservice or JavaScript Random Forest
   console.log(formData);
