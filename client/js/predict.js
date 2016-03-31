9a37638b43d28192ca67a0b40be538370b6e14d2
dionhagan <dionhagan@college.harvard.edu>
2016-03-30 21:47:59 -0400

    form data standardization completed

--- client/js/predict.js
+++ client/js/predict.js
@@ -1,43 +1,63 @@
 "use strict";
 
-var BASE_URL = "http://ws.chanceme.info/predict?admissionstest=0.976899206&AP=7&averageAP=1.06733864&SATsubject=0.324271565&GPA=-0.187109979&schooltype=0&intendedgradyear=2017&female=1&MinorityRace=0&international=0&sports=0&earlyAppl=0&alumni=0&outofstate=0&acceptrate=0.151&size=6621&public=0&finAidPct=0&instatePct=0&callback=getJSON";
-
-console.log(jsonData);
-
 function predict(formData) {
+
   // TODO: call webservice or JavaScript Random Forest
   console.log(formData);
-  // XXX: mock prediction generator. Remove later.
-  var predictions = [
-    {"college": "Princeton"},
-    {"college": "Harvard"},
-    {"college": "Yale"},
-    {"college": "Columbia"},
-    {"college": "Stanford"},
-    {"college": "UChicago"},
-    {"college": "MIT"},
-    {"college": "Duke"},
-    {"college": "UPenn"},
-    {"college": "CalTech"},
-    {"college": "JohnsHopkins"},
-    {"college": "Dartmouth"},
-    {"college": "Northwestern"},
-    {"college": "Brown"},
-    {"college": "Cornell"},
-    {"college": "Vanderbilt"},
-    {"college": "WashU"},
-    {"college": "Rice"},
-    {"college": "NotreDame"},
-    {"college": "UCB"},
-    {"college": "Emory"},
-    {"college": "Georgetown"},
-    {"college": "CarnegieMellon"},
-    {"college": "UCLA"},
-    {"college": "USC"}
-  ];
 
-  for (var i = 0; i < predictions.length; i++) {
-    predictions[i].prob = Math.random();
-  }
-  return predictions;
+  // base url for webservice request src
+  var BASE_URL = "http://ws.chanceme.info/predict?"
+
+  // object to convert ACT to SAT scores
+  var act2sat = {"36": 1600, "35": 1560, "34": 1510, "33": 1460, 
+               "32": 1420, "31": 1380, "30": 1340, "29": 1300, 
+               "28": 1260, "27": 1220, "26": 1190, "25": 1150, 
+               "24": 1110, "23": 1070, "22": 1030, "21": 990, 
+               "20": 950, "19": 910, "18": 870, "17": 830, "16": 790, 
+               "15": 740, "14": 690, "13": 640, "12": 590, "11": 530}
+
+  // means and standard deviations for standardizable values
+  var means, stds;
+
+  d3.csv('../client/data/normalize_means.csv', function(meanData) {
+    d3.csv('../client/data/normalize_stds.csv', function(stdData) {
+
+      means = meanData;
+      stds = stdData;
+
+      // standardize values
+      formData.apave = (formData.apave - means.averageAP) / stds.averageAP;
+      formData.gpa = (formData.gpa - means.GPA) / stds.GPA;
+      formData.sat2ave = (formData.sat2ave - means.SATsubject) / stds.SATsubject;
+
+      // calculate standard admissions test score
+      var admissionstest;
+
+      if (formData.act == 0 && formData.sat == 0) {
+        admissionstest = 0;
+      }
+      else if (formData.act != 0 && formData.sat == 0) {
+        admissionstest = (formData.sat - means.admissionstest) / std.admissionstest;
+      }
+      else if (formData.act == 0 && formData.sat != 0) {
+        admissionstest = (act2sat[formData.act] - means.admissionstest) / std.admissionstest;
+      }
+      else {
+        if (formData.sat > act2sat[formData.act]) {
+          admissionstest = (formData.sat - means.admissionstest) / std.admissionstest;
+        }
+        else {
+          admissionstest = (act2sat[formData.act] - means.admissionstest) / std.admissionstest;
+        }
+      }
+
+      // update script src url w/ new values
+
+    });
+  });
+
+
+  console.log(preds);
+
+  return preds;
 }
