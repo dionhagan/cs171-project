c88434d51579492db2ba50d79b01596a675972e2
dionhagan <dionhagan@college.harvard.edu>
2016-04-11 19:09:24 -0400

    updated to make XML HTTP request, giving a permission denied error

--- client/js/predict.js
+++ client/js/predict.js
@@ -6,8 +6,8 @@ function predict(formData) {
 
   //console.log(formData);
 
-  // base url for webservice request src
-  var BASE_URL = "http://ws.chanceme.info/predict?"
+  // url template for webservice request src
+  var url = "http://ws.chanceme.info/predict?admissionstest={TEST}&AP={AP}&averageAP={APAVE}&SATsubject={SAT2}&GPA={GPA}&schooltype={HS}&intendedgradyear=2017&female={GENDER}&MinorityRace=0&international=0&sports=0&earlyAppl=0&alumni=0&outofstate=0&acceptrate=0.151&size=6621&public=0&finAidPct=0&instatePct=0";
 
   // object to convert ACT to SAT scores
   var act2sat = {"36": 1600, "35": 1560, "34": 1510, "33": 1460, 
@@ -26,69 +26,97 @@ function predict(formData) {
       means = meanData[0];
       stds = stdData[0];
 
-      // standardize values
-      formData.apave = (formData.apave - +means.averageAP) / +stds.averageAP;
-      formData.gpa = (formData.gpa - +means.GPA) / +stds.GPA;
-      formData.sat2ave = (formData.sat2ave - +means.SATsubject) / +stds.SATsubject;
-
       // calculate standard admissions test score
       var admissionstest;
 
-      // take largest score and standardize
-      if (formData.act == 0 && formData.sat == 0) {
-        admissionstest = 0;
-      }
-      else if (formData.act != 0 && formData.sat == 0) {
-        admissionstest = (formData.sat - means.admissionstest) / stds.admissionstest;
-      }
-      else if (formData.act == 0 && formData.sat != 0) {
-        admissionstest = (act2sat[formData.act] - means.admissionstest) / stds.admissionstest;
-      }
-      else {
-        if (formData.sat > act2sat[formData.act]) {
-          admissionstest = (formData.sat - means.admissionstest) / stds.admissionstest;
+      // Get form selections
+      var sat = document.getElementById('sat');
+      var act = document.getElementById('act');
+      var gpa = document.getElementById('gpa');
+      var apave = document.getElementById('apave');
+      var apnum = document.getElementById('apnum');
+      var sat2ave = document.getElementById('sat2ave');
+      var hs = document.getElementById('hs');
+      var gender = document.getElementById('gender');
+      var race = document.getElementById('race');
+
+      var newurl;
+      var xhr = new XMLHttpRequest();
+
+      // Standardize values and construct new URL
+      function getNewURL(){
+
+        // Standardize SAT/ACT
+        // take larger score and standardize
+        if (act.value == 0 && sat.value == 0) {
+          admissionstest = 0;
+        }
+        else if (sat.value != 0 && act.value == 0) {
+          admissionstest = (sat.value - means.admissionstest) / stds.admissionstest;
+        }
+        else if (sat.value == 0 && act.value != 0) {
+          admissionstest = (act2sat[act.value] - means.admissionstest) / stds.admissionstest;
         }
         else {
-          admissionstest = (act2sat[formData.act] - means.admissionstest) / stds.admissionstest;
+          if (sat.value > act2sat[act.value]) {
+            admissionstest = (sat.value - means.admissionstest) / stds.admissionstest;
+          }
+          else {
+            admissionstest = (act2sat[act.value] - means.admissionstest) / stds.admissionstest;
+          }
         }
+
+        // Standardize GPA, Average AP, and SAT2
+        var gpaValue = (gpa.value - +means.GPA) / +stds.GPA;
+        var apaveValue = (apave.value - +means.averageAP) / +stds.averageAP;
+        var sat2aveValue = (sat2ave.value - +means.SATsubject) / +stds.SATsubject;
+
+        // Construct New URL
+        var result_url = url.replace("{TEST}", admissionstest)
+                            .replace("{GPA}", gpaValue)
+                            .replace("{AP}", apnum.value)
+                            .replace("{APAVE}", apaveValue)
+                            .replace("{SAT2}", sat2aveValue)
+                            .replace("{HS}", hs.value)
+                            .replace("{GENDER}", gender.value);
+                            //.replace("{RACE}", race.value);
+        return result_url;
       }
 
-      //console.log("formData: ");
-      //console.log(formData);
+      // Make Webservice Requests
+      function listen() {
+        newurl = getNewURL();
+        document.getElementById("result").innerHTML = "calling to webservice...<br>"+newurl;
+        xhr.open("GET", newurl);
+        xhr.send();
+      }
 
-      // build new request url
-      var new_src = BASE_URL + 
-                    "admissionstest=" + admissionstest + "&" +
-                    "AP=" + formData.apnum + "&" +
-                    "averageAP=" + formData.apave + "&" +
-                    "SATsubject=" + formData.sat2ave + "&" + 
-                    "GPA=" + formData.gpa + "&" +
-                    "schooltype=" + formData.hs + "&" +
-                    "intendedgradyear=2017&" +
-                    "female=" + formData.gender + "&" +
-                    "MinorityRace=0&" +
-                    "international=0&" +
-                    "sports=0&" +
-                    "earlyAppl=0&" +
-                    "alumni=0&" +
-                    "outofstate=0&" +
-                    "acceptrate=0.151&" +
-                    "size=6621&" +
-                    "public=0&" +
-                    "finAidPct=0&" +
-                    "instatePct=0&" +
-                    "callback=getJSON";
+      // Add Event Listeners
+      sat.addEventListener("change", listen(), false);
+      act.addEventListener("change", listen(), false);
+      gpa.addEventListener("change", listen(), false);
+      apave.addEventListener("change", listen(), false);
+      apnum.addEventListener("change", listen(), false);
+      sat2ave.addEventListener("change", listen(), false);
+      hs.addEventListener("change", listen(), false);
+      gender.addEventListener("change", listen(), false);
+      //race.addEventListener("change", listen(), false);
 
-      //console.log("new_src: ");
-      console.log(new_src);
-
-      // update src
-      $("#predictions").attr("src", new_src);
-
+      // Fetch JSON
+      xhr.onreadystatechange=function(){
+        if (this.readyState==4 && this.status==200){
+          var predictions = JSON.parse(this.response).preds;
+          var str = "";
+          for (var i = 0; i < predictions.length; i++) {
+            str += predictions[i].college + ": " + predictions[i].prob + "<br>\n";
+          }
+          document.getElementById("result").innerHTML = str;
+          //return predictions;
+        } else if (this.readyState != 1) {
+          document.getElementById("result").innerHTML =
+            "Ready state:"+this.readyState+" Status:"+this.status;
+        }
+      }
     });
   });
-
-  console.log(preds);
-
-  return preds;
 }
